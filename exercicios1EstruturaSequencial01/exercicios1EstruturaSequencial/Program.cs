11e8c908f3eaf67f5fcda25f071b79217b78fdb0
Pamela Caffa <caffa35@gmail.com>
2021-07-26 08:50:45 -0300

    Exercicio que faz a soma de 2 valores passados pelo usuario

---
+++ exercicios1EstruturaSequencial01/exercicios1EstruturaSequencial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicios1EstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite o primeiro número:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("SOMA = " + (num1 + num2) );
+        }
+    }
+}
