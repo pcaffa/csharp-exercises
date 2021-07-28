9d1ee43ed779c3a5a1f24e451c5ca1772860ddcc
Pamela Caffa <caffa35@gmail.com>
2021-07-27 23:11:15 -0300

    Exercicio que calcula a area de um circulo de acordo com o raio passado

---
+++ exercicios1EstruturaSequencial02/exercicios1EstruturaSequencial02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicios1EstruturaSequencial02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            double pi = 3.14159;
+            double area;
+
+            Console.WriteLine("Digite o raio do círculo:");
+            raio = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            area = pi * (Math.Pow(raio,2.0));
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
